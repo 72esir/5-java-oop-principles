337ec7ba447502d78d1a2d97cf9a2263ac2abc7a
72esir <asalamatin640@gmail.com>
2024-10-17 12:28:05 +0500

    Salamatin Aleksey Task04

---
+++ task04/src/com/example/task04/Logger.java
@@ -0,0 +1,89 @@
+package com.example.task04;
+
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+
+public class Logger {
+    private static final HashMap<String, Logger> loggers = new HashMap<>();
+    private final String Name;
+    private Level Level;
+
+    public Logger(String name){
+        Name = name;
+        Level = Level.INFO;
+        loggers.put(name, this);
+    }
+
+    public Logger(String name, Level level){
+        Name = name;
+        Level = level;
+        loggers.put(name, this);
+    }
+
+    public static Logger getLogger(String name){
+        if(loggers.get(name) == null){
+            loggers.put(name,new Logger(name));
+        }
+        return loggers.get(name);
+    }
+
+    public String getName(){
+        return this.Name;
+    }
+
+    public Level getLevel(){
+        return this.Level;
+    }
+
+    public void setLevel(Level level){
+        Level = level;
+    }
+
+    public void log(Level level, String message) {
+        if (Level.ordinal() <= level.ordinal()) {
+            String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd hh:mm:ss"));
+            String printMessage = MessageFormat.format("[{0}] {1} {2} - {3}", level, date, Name, message);
+            System.out.println(printMessage);
+        }
+    }
+
+    public void log(Level level, String format, Object... objects) {
+        if (Level.ordinal() <= level.ordinal()){
+            System.out.println(MessageFormat.format(format, objects));
+        }
+    }
+
+    public void debug(String message){
+        this.log(Level.DEBUG, message);
+    }
+
+    public void debug(String format, Object... objects){
+        this.log(Level.DEBUG, format, objects);
+    }
+
+    public void info(String mesage){
+        this.log(Level.INFO, mesage);
+    }
+
+    public void info(String format, Object... objects){
+        this.log(Level.INFO, format, objects);
+    }
+
+    public void warning(String message){
+        this.log(Level.WARNING, message);
+    }
+
+    public void warning(String format, Object... objects){
+        this.log(Level.WARNING, format, objects);
+    }
+
+    public void error(String message){
+        this.log(Level.ERROR, message);
+    }
+
+    public void error(String format, Object... objects){
+        this.log(Level.ERROR, format, objects);
+    }
+}
--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,7 +1,32 @@
 package com.example.task04;
 
+import java.time.temporal.ChronoUnit;
+
 public class Task04Main {
     public static void main(String[] args) {
+        // Создаем обработчики для логирования
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        FileHandler fileHandler = new FileHandler("logs/app.log");
+        RotationFileHandler rotationFileHandler = new RotationFileHandler("logs/rotatedLog_", ChronoUnit.HOURS);
 
+        MemoryHandler memoryHandler = new MemoryHandler(3, consoleHandler, fileHandler);
+
+        // Создаем Logger с уровнем INFO, который использует обработчики console и file
+        Logger logger = new Logger("AppLogger", Level.DEBUG);
+
+
+        memoryHandler.log("First message");
+        memoryHandler.log("Second message");
+        memoryHandler.log("Third message");
+        memoryHandler.log("Fourth message");
+
+        // Логгируем разные уровни
+        logger.debug("This is a DEBUG message");
+        logger.info("This is an INFO message");
+        logger.warning("This is a WARNING message");
+        logger.error("This is an ERROR message");
+
+        rotationFileHandler.log("This is a log for rotated file.");
     }
 }
+
